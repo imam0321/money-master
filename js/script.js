d2963d7598290946fa1282e162199efbf0936fc1
imam0321 <imam.hossain0321@gmail.com>
2023-05-11 02:36:36 +0600

    complited a project

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 document
   .getElementById("btn-calculate-income")
-  .addEventListener("click", function () {
+  .addEventListener("click", function calculate() {
     const incomeValue = getInputValues("income-input-field");
     const foodValue = getInputValues("food-input-field");
     const rentValue = getInputValues("rent-input-field");
     const clothesValue = getInputValues("clothes-input-field");
     const expensesTotal = foodValue + rentValue + clothesValue;
-    const expensesValues = getValues("expenses-total");
     const totalExpense = setValues("expenses-total", expensesTotal);
     const currentBalance = incomeValue - totalExpense;
     const currenTotaltBalance = setValues("current-belance", currentBalance);
 
-    // console.log(updateBalance);
+    document
+      .getElementById("saving-btn")
+      .addEventListener("click", function () {
+        const savingParsent = getInputValues("Saving-input-field");
+        const totalSaving = (savingParsent * incomeValue) / 100;
+        const savingsTotalAmount = setValues("saving-amount", totalSaving);
+        const remainingAmount = currenTotaltBalance - savingsTotalAmount;
+        setValues("remaining-balance", remainingAmount);
+      });
   });
--- js/utility.js
+++ js/utility.js
@@ -3,11 +3,6 @@ function getInputValues(inputId) {
   const inputValue = parseFloat(inputField.value);
   return inputValue;
 }
-function getValues(valuesId) {
-  const values = document.getElementById(valuesId);
-  const previousValues = parseFloat(values.innerText);
-  return previousValues;
-}
 function setValues(valueId, result) {
   const results = document.getElementById(valueId);
   results.innerText = result;
